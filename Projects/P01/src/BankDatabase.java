d1c4434a9f3536063769d464039f28372a60c18b
Andre Fonseca <afbfonseca@gmail.com>
2018-04-17 20:54:06 +0100

    Projects folder clean up

--- Projects/P01/src/BankDatabase.java
+++
@@ -1,9 +0,0 @@
-import database.Database;
-import org.w3c.dom.Document;
-
-public final class BankDatabase extends Database {
-
-    public BankDatabase(Document document, String xsd_path) {
-        super(document, xsd_path);
-    }
-}
--- Projects/P01/src/BankGate.java
+++
@@ -1,9 +0,0 @@
-import server.GateKeeper;
-import server.Server;
-
-class BankGate extends GateKeeper {
-
-    public BankGate(Server server, int port) {
-        super(server, port);
-    }
-}
