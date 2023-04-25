201f641f50b00adb2f0f0a4e8b7ace04c1ec35ca
André Fonseca <afbfonseca@gmail.com>
2023-04-25 20:32:49 +0100

    Add comments

--- pt.isel.leim.icd/connectfour/src/models/Board.java
+++ pt.isel.leim.icd/connectfour/src/models/Board.java
@@ -1,18 +1,22 @@
 package models;
 
+/**
+ * Represents the Connect Four board in the game.
+ */
 public class Board {
 
-    private final Token[][] tokens;
-    private final int totalRows;
-    private final int totalColumns;
+    private final Token[][] tokens = new Token[6][7];
+    private final int totalRows = tokens.length;
+    private final int totalColumns = tokens[0].length;
 
-    public Board() {
-        tokens = new Token[6][7];
-        totalRows = tokens.length;
-        totalColumns = tokens[0].length;
-    }
-
-    public int dropToken(int column, Token token) {
+    /**
+     * Drops a token into the specified column if the column is not full. If the column is full, a RuntimeException is
+     * thrown.
+     * @param column The column to drop the token into in a range of 1 to 7.
+     * @param token The token to drop into the column.
+     * @return The row the token was dropped into.
+     */
+    public int dropToken(int column, Token token) throws RuntimeException {
         column -= 1;
         for (int row = totalRows - 1; row >= 0; row--) {
             if (tokens[row][column] == null) {
@@ -25,6 +29,12 @@ public class Board {
         throw new RuntimeException("Column is full");
     }
 
+    /**
+     * Returns the token at the specified row and column.
+     * @param row The row of the token in a range of 1 to 6.
+     * @param column The column of the token in a range of 1 to 7.
+     * @return The token at the specified row and column.
+     */
     public Token getToken(int row, int column) {
         row -= 1;
         column -= 1;
