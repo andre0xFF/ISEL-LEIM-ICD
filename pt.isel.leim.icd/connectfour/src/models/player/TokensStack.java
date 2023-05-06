a10480bd4fe65bdb3b4152ac42ee1fbec775693b
André Fonseca <afbfonseca@gmail.com>
2023-05-06 10:56:00 +0100

    Add comments

--- pt.isel.leim.icd/connectfour/src/models/player/TokensStack.java
+++ pt.isel.leim.icd/connectfour/src/models/player/TokensStack.java
@@ -21,26 +21,55 @@ public class TokensStack {
         this.color = color;
     }
 
+    /**
+     * Returns the number of tokens in the stack
+     *
+     * @return the number of tokens in the stack
+     */
     public int size() {
         return this.tokens.size();
     }
 
+    /**
+     * Returns true if the stack is empty
+     *
+     * @return true if the stack is empty
+     */
     public boolean isEmpty() {
         return this.tokens.isEmpty();
     }
 
+    /**
+     * Adds a token to the stack
+     *
+     * @param token the token to add
+     * @return true if the token was added
+     */
     public boolean add(Token token) {
         return this.tokens.add(token);
     }
 
+    /**
+     * Removes a token from the stack
+     *
+     * @return the removed token
+     */
     public Token pop() {
         return this.tokens.remove(0);
     }
 
+    /**
+     * Clears all tokens from the stack
+     */
     public void clear() {
         this.tokens.clear();
     }
 
+    /**
+     * Returns the color of the stack
+     *
+     * @return the color of the stack
+     */
     public Color color() {
         return this.color;
     }
