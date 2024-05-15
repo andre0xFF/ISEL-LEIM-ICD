18ad9bb46c9dce170dd73c0b5ebe74b84ab077b7
André Fonseca <a39758@alunos.isel.pt>
2024-05-15 10:11:20 +0100

    Refactor StartClientView

--- othello/src/main/java/pt/isel/icd/user/management/StartClientView.java
+++ othello/src/main/java/pt/isel/icd/user/management/StartClientView.java
@@ -1,114 +1,133 @@
 package pt.isel.icd.user.management;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * View with login and registration options.
  */
 public class StartClientView {
 
-    private JTextField username;
-    private JLabel passwordLabel;
+    private final UserClientController userClientController;
+
+    private JTextField usernameField;
     private JPasswordField passwordField;
+    private JLabel notificationLabel;
 
     public StartClientView(JFrame frame, UserClientController userClientController) {
+        this.userClientController = userClientController;
+
+        JPanel startViewPanel = createStartViewPanel();
+        frame.add(startViewPanel);
+    }
+
+    private JPanel createStartViewPanel() {
         JPanel startViewPanel = new JPanel();
-        GridBagLayout bagLayout = new GridBagLayout();
+
+        startViewPanel.setLayout(new GridBagLayout());
+
         GridBagConstraints constraints = new GridBagConstraints();
 
-        startViewPanel.setLayout(bagLayout);
+        addUsernameField(startViewPanel, constraints);
+        addPasswordField(startViewPanel, constraints);
+        addLoginButton(startViewPanel, constraints);
+        addSignUpButton(startViewPanel, constraints);
+        addNotificationLabel(startViewPanel, constraints);
 
-//        constraints.gridx = 0;
-//        constraints.gridy = 0;
-//        constraints.anchor = GridBagConstraints.WEST;
-//        constraints.insets.top = 10;
+        return startViewPanel;
+    }
 
+    private void addUsernameField(JPanel panel, GridBagConstraints constraints) {
         JLabel usernameLabel = new JLabel("Username");
         constraints.weighty = 0;
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.anchor = GridBagConstraints.WEST;
 
-        startViewPanel.add(usernameLabel, constraints);
+        panel.add(usernameLabel, constraints);
 
-        username = new JTextField(20);
-//        nickLabel.setEnabled(false);
+        usernameField = new JTextField(20);
         constraints.gridwidth = 2;
         constraints.gridx = 1;
         constraints.gridy = 0;
         constraints.anchor = GridBagConstraints.EAST;
 
-        startViewPanel.add(username, constraints);
+        panel.add(usernameField, constraints);
+    }
 
-        passwordLabel = new JLabel("Password");
-
+    private void addPasswordField(JPanel panel, GridBagConstraints constraints) {
+        JLabel passwordLabel = new JLabel("Password");
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.anchor = GridBagConstraints.WEST;
 
-        startViewPanel.add(passwordLabel, constraints);
+        panel.add(passwordLabel, constraints);
 
         passwordField = new JPasswordField(20);
-        passwordField.setEnabled(false);
+        passwordField.setEnabled(true);
         constraints.gridx = 1;
         constraints.gridy = 1;
         constraints.anchor = GridBagConstraints.EAST;
 
-        startViewPanel.add(passwordField, constraints);
+        panel.add(passwordField, constraints);
+    }
+
+    private void addLoginButton(JPanel panel, GridBagConstraints constraints) {
+        JButton loginButton = new JButton("Login");
+
+        loginButton.addActionListener(this::handleLoginAction);
+        loginButton.setPreferredSize(new Dimension(100, 40));
 
         constraints.gridwidth = 1;
         constraints.gridx = 1;
         constraints.gridy = 2;
         constraints.anchor = GridBagConstraints.CENTER;
         constraints.insets.top = 10;
-        JButton startLoginButton = new JButton("Log In");
 
-        startLoginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO: inform controller to change current panel to LogIn JPanel
+        panel.add(loginButton, constraints);
+    }
 
-//                controller.updateView(frame, (OthelloView) getTransitionMachine().getNextState(Event.CLICK_LOGIN));
+    private void addSignUpButton(JPanel panel, GridBagConstraints constraints) {
+        JButton startSignUpButton = new JButton("Sign Up");
 
-            }
-        });
-
-        startViewPanel.add(startLoginButton, constraints);
-
-        startLoginButton.setPreferredSize(new Dimension(100, 40));
+        startSignUpButton.addActionListener(this::handleSignUpAction);
+        startSignUpButton.setPreferredSize(new Dimension(100, 40));
 
         constraints.gridx = 2;
         constraints.gridy = 2;
         constraints.anchor = GridBagConstraints.CENTER;
 
-        JButton startSignUpButton = new JButton("Sign Up");
-        startSignUpButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO: inform controller to change current panel to SignUp JPanel
-//                controller.updateView(frame, (OthelloView) getTransitionMachine().getNextState(Event.CLICK_SIGNUP));
-            }
-        });
+        panel.add(startSignUpButton, constraints);
+    }
 
-//        constraints.gridx = 0;
-//        constraints.gridy = 1;
-        constraints.anchor = GridBagConstraints.EAST;
+    private void addNotificationLabel(JPanel panel, GridBagConstraints constraints) {
+        notificationLabel = new JLabel();
 
-        startViewPanel.add(startSignUpButton, constraints);
-        startSignUpButton.setPreferredSize(new Dimension(100, 40));
+        notificationLabel.setForeground(Color.red);
 
         constraints.gridx = 1;
         constraints.gridy = 3;
         constraints.gridwidth = 3;
         constraints.anchor = GridBagConstraints.CENTER;
-        JLabel displayMessage = new JLabel("Message asdasdawqwe");
-        displayMessage.setForeground(Color.red);
-        startViewPanel.add(displayMessage, constraints);
 
-        frame.add(startViewPanel);
-//        frame.setVisible(true);
+        panel.add(notificationLabel, constraints);
+    }
+
+    private void handleLoginAction(ActionEvent e) {
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+
+        try {
+            userClientController.authenticate(new User(username, password));
+        } catch (JsonProcessingException ex) {
+            notificationLabel.setText("Error authenticating user");
+        }
+    }
+
+    private void handleSignUpAction(ActionEvent e) {
+        //TODO: inform controller to change current panel to SignUp JPanel
     }
 }
